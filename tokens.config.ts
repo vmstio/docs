adaf0856b99ca9a149e4eef0803d080a8856678a
Michael Stanclift <mx@vmstan.com>
2024-07-30 13:38:32 -0500

    New colors

--- tokens.config.ts
+++ tokens.config.ts
@@ -3,15 +3,15 @@ import { defineTheme } from 'pinceau'
 export default defineTheme({
   color: {
     primary: {
-      50: '#F1FCFF',
-      100: '#DCF7FF',
-      200: '#C5F2FF',
-      300: '#82E3FF',
-      400: '#55E1FF',
-      500: '#1AD6FF',
-      600: '#09A0C1',
-      700: '#024757',
-      800: '#00232B',
+      50: '#E8E9FF',
+      100: '#C5C6FF',
+      200: '#A2A3FF',
+      300: '#7F80FF',
+      400: '#5C5DFF',
+      500: '#6364FF',
+      600: '#563ACC',
+      700: '#2F0C7A',
+      800: '#17063B',
       900: '#001A1F'
     }
   }
